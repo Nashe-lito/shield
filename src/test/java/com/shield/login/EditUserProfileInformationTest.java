f7e52b06e00c16482727f07ac3f2d6b8fca0ccb3
Nashe-lito <krevetk@ukr.net>
2020-07-10 16:44:20 +0300

    Adding new class EditUserProfileInformationTest

---
+++ src/test/java/com/shield/login/EditUserProfileInformationTest.java
@@ -0,0 +1,11 @@
+package com.shield.login;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class EditUserProfileInformationTest extends WebDriverSettings {
+  @Test
+  public void test() {
+
+  }
+}
